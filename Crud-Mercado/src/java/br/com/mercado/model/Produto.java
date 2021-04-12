359e75c1d42db14960e496502a3afa0b13d8c485
joelson-dev-joe <joelson.dh@gmail.com>
2021-04-12 19:33:11 -0300

    Criando Class Produto e atributos

--- Crud-Mercado/src/java/br/com/mercado/model/Produto.java
+++ Crud-Mercado/src/java/br/com/mercado/model/Produto.java
@@ -11,4 +11,35 @@ package br.com.mercado.model;
  */
 public class Produto {
     
+    private int codigo_produto;
+    private String descricao_produto;
+    private Double preco_produto;
+
+    public int getCodigo_produto() {
+        return codigo_produto;
+    }
+
+    public void setCodigo_produto(int codigo_produto) {
+        this.codigo_produto = codigo_produto;
+    }
+
+    public String getDescricao_produto() {
+        return descricao_produto;
+    }
+
+    public void setDescricao_produto(String descricao_produto) {
+        this.descricao_produto = descricao_produto;
+    }
+
+    public Double getPreco_produto() {
+        return preco_produto;
+    }
+
+    public void setPreco_produto(Double preco_produto) {
+        this.preco_produto = preco_produto;
+    }
+    
+    
+    
+    
 }
