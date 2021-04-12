029e866472f37bb113c183923a6023b678f92374
joelson-dev-joe <joelson.dh@gmail.com>
2021-04-12 19:30:13 -0300

    Criando Class ConnectionFactory

---
+++ Crud-Mercado/src/java/br/com/mercado/dao/ProdutoDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mercado.dao;
+
+import br.com.mercado.model.Produto;
+
+/**
+ *
+ * @author Joelson Correia
+ */
+public class ProdutoDAO {
+    
+    public ProdutoDAO(){
+    }
+    
+    public void inserir(Produto produto){
+        
+    }
+}
